82345a8e9f9ba3c643193fc654dbc73b751cf8b2
jukgol <40035477+jukgol@users.noreply.github.com>
2026-02-01 13:11:19 +0900

    Remove Python related files and update package

--- python/app/ui/listtap/download/func.py
+++
@@ -1,6 +0,0 @@
-def _update_list_panel(panel, data):
-    """SmartListPanel에 데이터를 안전하게 채우는 헬퍼 함수"""
-    if isinstance(data, list):
-        panel.set_list(data)
-    else:
-        panel.set_list([str(data)])
